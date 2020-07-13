49f79d1bf3d09c8a1ada67addeefa3d565cb1013
hilmi <hilmi.coskun@ibm.com>
2020-07-13 15:57:18 +0300

    third

--- Hello.js
+++ Hello.js
@@ -1,3 +1,4 @@
 console.log("Arasalar muhtemelen gitmeyeceğim /n");
 console.log("Der Murat Hoca. Ön koşul 25.000 ve yukarısıdır /n");
 console.log("Hep bunlar oylee boyle  /n");
+console.log("Hep bunlar 123456  /n");
